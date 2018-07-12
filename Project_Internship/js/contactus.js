3c29c67454c3e17ad75baebe1680bec7c35f04bd
Gandhi Shaili <rinu21990@gmail.com>
2018-07-12 23:49:14 +0530

    Music_School_Website

--- Project_Internship/js/contactus.js
+++ Project_Internship/js/contactus.js
@@ -1,15 +1,16 @@
 function showmenu(){
-    for(i=0;i<5;i++){
+    for(var i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
-	 y.getElementsByTagName("a")[0].style.display="block";
+	 y.style.display="block";
+	 y.getElementsByTagName("a")[0].style.display="block";	 
 	}
 document.getElementsByClassName("menu")[0].style.display = "none";
 document.getElementsByClassName("close")[0].style.display = "block";
  setInterval(function(){
  if(document.body.clientWidth>1200){
 	 document.getElementsByClassName("menu")[0].style.display = "none";
-     document.getElementsByClassName("close")[0].style.display = "none";	 
+     document.getElementsByClassName("close")[0].style.display = "none";	
   }
   else{
   if( (y.getElementsByTagName("a")[0].style.display)=="block"){
@@ -26,15 +27,17 @@ function closemenu(){
    for(i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
+	 y.style.display="none";
 	 y.getElementsByTagName("a")[0].style.display="none";
 	}
 document.getElementsByClassName("menu")[0].style.display = "block";
-document.getElementsByClassName("close")[0].style.display = "none";	
+document.getElementsByClassName("close")[0].style.display = "none";		
 setInterval(function(){if(document.body.clientWidth>1200){
  for(i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
+	 y.style.display="block";
 	 y.getElementsByTagName("a")[0].style.display="block";
 	}
-}},100);	
+}},100);
 }
--- Project_Internship/js/home.js
+++ Project_Internship/js/home.js
@@ -1,8 +1,9 @@
 function showmenu(){
-    for(i=0;i<5;i++){
+    for(var i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
-	 y.getElementsByTagName("a")[0].style.display="block";
+	 y.style.display="block";
+	 y.getElementsByTagName("a")[0].style.display="block";	 
 	}
 document.getElementsByClassName("menu")[0].style.display = "none";
 document.getElementsByClassName("close")[0].style.display = "block";
@@ -26,6 +27,7 @@ function closemenu(){
    for(i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
+	 y.style.display="none";
 	 y.getElementsByTagName("a")[0].style.display="none";
 	}
 document.getElementsByClassName("menu")[0].style.display = "block";
@@ -34,6 +36,7 @@ setInterval(function(){if(document.body.clientWidth>1200){
  for(i=0;i<5;i++){
      var x=document.getElementsByClassName("header")[0];
 	 var y=  x.getElementsByTagName("li")[i];
+	 y.style.display="block";
 	 y.getElementsByTagName("a")[0].style.display="block";
 	}
 }},100);
